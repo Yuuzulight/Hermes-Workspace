77e487282b754eef1456e5181c69db7e2c017c50
Yuuzulight <yuuzulight@gmail.com>
2026-09-02 03:08:04 +0800

    Clean up deleted dashboard files from index

--- dashboard/creator-libs/scripts/build-pipeline.mjs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import { build } from "esbuild-wasm";
-await build({
-  entryPoints: ["src/initial.tsx"],
-  bundle: true,
-  platform: "browser",
-  target: ["es2018"],
-  outfile: "../static/bundle.js",
-});
